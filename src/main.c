7e1c0c68185fba5fc35c0a15a11657207cd01f7b
Todd Cope <todd@t3-i.com>
2021-12-15 16:06:17 -0600

    Add shading. Implement z-sort.

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 
 #define _T3LOGO_TARGET_TILT  (0.5)
 #define _T3LOGO_TARGET_ANGLE (ALLEGRO_PI * 2.25)
+#define _T3LOGO_LIGHT_ANGLE  (ALLEGRO_PI * 1.25)
 
 /* shape constructed of triplets of vertices */
 typedef struct
@@ -13,6 +14,8 @@ typedef struct
 	int vertex_count;
 
 	ALLEGRO_VERTEX transformed_vertex[_T3LOGO_MAX_VERTICES];
+	float z_depth;
+	float angle;
 
 } SHAPE;
 
@@ -21,6 +24,7 @@ typedef struct
 {
 
 	SHAPE logo_side[3];
+	SHAPE * logo_side_zsort[3];
 	float angle;
 	float tilt;
 	float v_tilt;   // tilt velocity
@@ -30,6 +34,14 @@ typedef struct
 	int tick;
 
 } APP_INSTANCE;
+
+static int side_z_depth_qsort_proc(const void * p1, const void * p2)
+{
+	SHAPE * side1 = *(SHAPE **)p1;
+	SHAPE * side2 = *(SHAPE **)p2;
+
+	return side2->z_depth < side1->z_depth;
+}
 
 static void init_shape(SHAPE * sp)
 {
@@ -44,6 +56,38 @@ static void add_vertex(SHAPE * sp, float x, float y, float z)
 	sp->vertex_count++;
 }
 
+static float linear_spread(float angle_1, float angle_2, float val_min, float val_max)
+{
+	float d = fabs(val_max - val_min);
+	float d_angle = angle_2 - angle_1;
+
+	if(d_angle < 0)
+	{
+		d_angle += ALLEGRO_PI;
+	}
+	d_angle = fmod(d_angle, ALLEGRO_PI); // loop at ALLEGRO_PI intervals
+	d_angle -= ALLEGRO_PI / 2.0; // subtract 90 degrees
+	if(d_angle < 0)
+	{
+		return val_min - d * (d_angle / (ALLEGRO_PI / 2.0));
+	}
+	return val_min + d * (d_angle / (ALLEGRO_PI / 2.0));
+}
+
+static ALLEGRO_COLOR get_lit_color(ALLEGRO_COLOR base_color, float light_angle, float side_normal_angle)
+{
+	float r, g, b;
+	float h, s, l;
+	float target_l;
+
+	al_unmap_rgb_f(base_color, &r, &g, &b);
+	al_color_rgb_to_hsl(r, g, b, &h, &s, &l);
+	target_l = linear_spread(light_angle, side_normal_angle, l / 2.0, l);
+//	target_l = (1.0 - fmod(fabs(light_angle - side_normal_angle), ALLEGRO_PI) / ALLEGRO_PI) * l;
+
+	return al_color_hsl(h, s, target_l);
+}
+
 static void set_shape_orientation(SHAPE * sp, float ox, float oy, float angle, float tilt, float scale)
 {
 	int i;
@@ -51,15 +95,20 @@ static void set_shape_orientation(SHAPE * sp, float ox, float oy, float angle, f
 	float vdistance;
 	float temp_z;
 
+	sp->z_depth = 1000000;
 	for(i = 0; i < sp->vertex_count; i++)
 	{
 		vangle = atan2(sp->vertex[i].z, sp->vertex[i].x);
 		vdistance = hypot(sp->vertex[i].x, sp->vertex[i].z);
 		temp_z = sin(vangle + angle) * vdistance;
+		if(temp_z < sp->z_depth)
+		{
+			sp->z_depth = temp_z;
+		}
 		sp->transformed_vertex[i].x = ox + cos(vangle + angle) * vdistance * scale;
 		sp->transformed_vertex[i].y = oy + (sp->vertex[i].y + temp_z * tilt) * scale;
 		sp->transformed_vertex[i].z = 0;
-		sp->transformed_vertex[i].color = t3f_color_black;
+		sp->transformed_vertex[i].color = get_lit_color(al_map_rgb(46, 104, 158), _T3LOGO_LIGHT_ANGLE, sp->angle);
 	}
 }
 
@@ -156,8 +205,12 @@ void app_logic(void * data)
 		t3f_key[ALLEGRO_KEY_DOWN] = 0;
 	}
 	set_shape_orientation(&app->logo_side[0], 256, 256, app->angle, app->tilt, 48);
+	app->logo_side[0].angle = app->angle + ALLEGRO_PI / 2.0;
 	set_shape_orientation(&app->logo_side[1], 256, 256, app->angle, app->tilt, 48);
+	app->logo_side[1].angle = app->angle + ALLEGRO_PI;
 	set_shape_orientation(&app->logo_side[2], 256, 256, app->angle, app->tilt, 48);
+	app->logo_side[2].angle = app->angle + ALLEGRO_PI * 1.5;
+	qsort(app->logo_side_zsort, 3, sizeof(SHAPE *), side_z_depth_qsort_proc);
 }
 
 /* main rendering routine */
@@ -166,9 +219,9 @@ void app_render(void * data)
 	APP_INSTANCE * app = (APP_INSTANCE *)data;
 
 	al_clear_to_color(t3f_color_white);
-	render_shape(&app->logo_side[0]);
-	render_shape(&app->logo_side[1]);
-	render_shape(&app->logo_side[2]);
+	render_shape(app->logo_side_zsort[0]);
+	render_shape(app->logo_side_zsort[1]);
+	render_shape(app->logo_side_zsort[2]);
 }
 
 /* initialize our app, load graphics, etc. */
@@ -222,6 +275,10 @@ bool app_initialize(APP_INSTANCE * app, int argc, char * argv[])
 	add_vertex(&app->logo_side[2], 1, -1, 3);
 	add_vertex(&app->logo_side[2], 1, 3, 3);
 	add_vertex(&app->logo_side[2], -1, 3, 3);
+
+	app->logo_side_zsort[0] = &app->logo_side[0];
+	app->logo_side_zsort[1] = &app->logo_side[1];
+	app->logo_side_zsort[2] = &app->logo_side[2];
 
 	app->tick = -1000;
 
